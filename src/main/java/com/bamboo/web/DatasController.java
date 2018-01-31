a73c513e18aabad4880539916acfb7f487a3da2f
hzwangzi123456 <591597278@qq.com>
2018-02-01 04:20:11 +0800

    第一次委托到本地仓库

---
+++ src/main/java/com/bamboo/web/DatasController.java
@@ -0,0 +1,179 @@
+package com.bamboo.web;
+
+import com.alibaba.fastjson.JSON;
+import com.bamboo.bo.DatasBo;
+import com.bamboo.vo.Datas;
+import com.common.base.BaseController;
+import com.common.util.ExportExcelUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @author wangzi
+ * @date 18/1/25 上午5:23.
+ */
+@Controller
+@Scope("prototype")
+@RequestMapping("/DatasController")
+public class DatasController extends BaseController {
+    private static Logger logger = Logger
+            .getLogger(DatasController.class);
+
+
+    /**
+     * 注入业务接口层
+     **/
+    @Autowired
+    private DatasBo datasBo;
+
+    /**
+     * 定义参数
+     **/
+    private Datas datas;
+
+    @ModelAttribute
+    public void setParaVal(Datas datas) {
+        this.datas = datas;
+        System.out.println(JSON.toJSONString(datas).toString());
+    }
+
+    public DatasBo getDatasBo() {
+        return datasBo;
+    }
+
+    public Map getPojoMap() {
+        Map map = new HashMap<String, Object>();
+        map.put("airTemperature", datas.getAirTemperature());
+        map.put("airHumidity", datas.getAirHumidity());
+        map.put("carbonDioxide", datas.getCarbonDioxide());
+        map.put("soilTemperature", datas.getSoilTemperature());
+        map.put("soilHumidity", datas.getSoilHumidity());
+        map.put("insectDensity", datas.getInsectDensity());
+        map.put("dateTime", datas.getDateTime());
+        map.put("startTimeTIMESTAMP", datas.getStartTimeTIMESTAMP());
+        map.put("endTimeTIMESTAMP", datas.getEndTimeTIMESTAMP());
+        return map;
+    }
+
+    @RequestMapping("/insertDatas.do")
+    public void insertDatas() {
+        if (datas == null) {
+            setFailure("未接受到数据");
+            return;
+        }
+        if (datas.getAirHumidity() == null) {
+            setFailure("未设置空气湿度");
+            return;
+        }
+        if (datas.getAirTemperature() == null) {
+            setFailure("未设置空气温度");
+            return;
+        }
+        if (datas.getCarbonDioxide() == null) {
+            setFailure("未设置二氧化碳含量");
+            return;
+        }
+        if (datas.getInsectDensity() == null) {
+            setFailure("未设置虫口密度");
+            return;
+        }
+        if (datas.getSoilHumidity() == null) {
+            setFailure("未设置土壤湿度");
+            return;
+        }
+        if (datas.getSoilTemperature() == null) {
+            setFailure("未设置土壤温度");
+            return;
+        }
+        try {
+            int res = datasBo.insertDatas(datas);
+            if (res == 1) {
+                setSuccess("插入一条记录");
+            } else {
+                setFailure("插入失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.toString());
+        }
+    }
+
+    @RequestMapping("/findDatasCountByVo.do")
+    public void findDatasCountByVo() {
+        try {
+            Integer i = datasBo.findDatasCountByVo(datas);
+            System.out.println(i);
+            setAjaxMsg(i.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.toString());
+        }
+    }
+
+    @RequestMapping("/findDatasByPojo.do")
+    public void findDatasByPojo() {
+        try {
+            List<Datas> list = datasBo.findDatasByPojo(datas);
+            setAjaxObject(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.toString());
+        }
+    }
+
+    @RequestMapping("/findDatasByPage.do")
+    public void findDatasByPage() {
+        try {
+            Integer cnt = datasBo.findDatasCountByVo(datas);
+            getPageUtil().setTotal(cnt);
+            List<Datas> list = null;
+            if (cnt > 0) {
+                Map schMap = getSchMap();
+                schMap.putAll(getPojoMap());
+                list = datasBo.findDatasByPage(schMap);
+                setAjaxGridData(list);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.toString());
+        }
+    }
+
+    @RequestMapping(value = "/generateExcel.do")
+    public void gengerateExcel() {
+        try {
+            //定义导出excel的名字
+            String excelName = "竹林数据表" + datas.getStartTimeTIMESTAMP().substring(0, 10) + "to" + datas.getEndTimeTIMESTAMP().substring(0, 10);
+
+            //得到所要导出的数据
+            List<Datas> list = datasBo.findDatasByPojo(datas);
+
+            // 获取需要转出的excle表头的map字段
+            LinkedHashMap<String, String> fieldMap = new LinkedHashMap<String, String>();
+            fieldMap.put("airTemperature", "空气温度");
+            fieldMap.put("airHumidity", "空气湿度");
+            fieldMap.put("carbonDioxide", "二氧化碳含量");
+            fieldMap.put("soilTemperature", "土壤温度");
+            fieldMap.put("soilHumidity", "土壤湿度");
+            fieldMap.put("insectDensity", "虫口密度");
+            fieldMap.put("dateTime", "日期");
+
+
+            ExportExcelUtils.export(excelName, list, fieldMap, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.toString());
+        }
+
+    }
+}
