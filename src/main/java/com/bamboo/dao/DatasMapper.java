27eb03053ba85838ca57b80616861eb6442da087
hzwangzi123456 <591597278@qq.com>
2018-02-02 21:52:39 +0800

    commit

---
+++ src/main/java/com/bamboo/dao/DatasMapper.java
@@ -0,0 +1,12 @@
+package com.bamboo.dao;
+
+import com.bamboo.vo.Datas;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author wangzi
+ * @date 18/1/25 上午5:05.
+ */
+@Repository
+public interface DatasMapper extends GenericMapper<Datas> {
+}
